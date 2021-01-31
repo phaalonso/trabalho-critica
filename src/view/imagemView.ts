3ffff36784e13de894a24bc78d949a8fe7aa1bd7
phaalonso <phaalonso@gmail.com>
2021-01-31 01:26:13 -0300

    Alteração em imagemView

--- src/view/imagemView.ts
+++ src/view/imagemView.ts
@@ -1,13 +1,13 @@
-import Imagem from "../models/Imagem";
+import Image from "../models/Image";
 
 export default {
-    render(img: Imagem) {
+    render(img: Image) {
         return {
             id: img.id,
             url: `http://localhost:3333/uploads/${img.path}`
         }
     },
-    renderMany(imgs: Imagem[]) {
+    renderMany(imgs: Image[]) {
         return imgs.map(img => this.render(img));
     }
 }
